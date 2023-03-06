da606132561695633fd010e305c20753530252f4
pkocev49 <120195818+pkocev49@users.noreply.github.com>
2023-03-07 00:53:28 +0100

    HOME WORK 04

---
+++ CLASS 04 BS NODE.JS/file-sevice.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+const writeToFile = (path, data) => {
+  fs.writeFileSync(path, data);
+};
+
+const readFromFile = (path) => {
+  const content = JSON.parse(fs.readFileSync(path, { encoding: "utf-8" }));
+  return content;
+};
+
+const appendToFile = (path, data) => {
+  fs.appendFileSync(path, data);
+};
+
+export default {
+  writeToFile,
+  readFromFile,
+  appendToFile,
+};
